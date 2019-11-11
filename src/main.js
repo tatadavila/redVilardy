0b89e03edab0509bd4ae40e93c9116c8a9654bb0
your usernametatadavila <tata.davila.a@gmail.com>
2019-11-10 19:28:53 -0500

    Start component ereased.<Content pasted inside home view

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 Vue.component("Navbar", require("./components/Navbar.vue").default);
-Vue.component("Start", require("./components/Start.vue").default);
 Vue.component("ContentTable", require("./components/ContentTable.vue").default);
 Vue.component("Topic", require("./components/Topic.vue").default);
 Vue.component("Topic2", require("./components/Topic2.vue").default);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,75 @@
 <template>
   <div class="home">
-    <Start></Start>
+    <div id="startContainer" class="container">
+      <div
+        class="row justify-content-xl-center justify-content-md-left justif-content-sm-center"
+      >
+        <div class="col-xl-6 col-4">
+          <h2 id="welc">BIENVENIDOS</h2>
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-xl-6 col-8">
+          <img
+            src="../assets/img/evaluate.png"
+            class="img-fluid"
+            id="evaluate"
+          />
+        </div>
+        <div id="containerText" class="col-xl-6 col-8">
+          <div id="text">
+            <p id="paragraph" align="justify">
+              Este recurso fue creado para que los estudiantes tengan una mayor
+              interpretación de la Evaluación de un Proyecto de Inversión, donde
+              se identificaran aspectos y conceptos como: Oportunidad de
+              Inversión y el Estudio del Mercado, Estudio Técnico, Estudio
+              Económico y Evaluación Económica Financiera.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Start from "@/components/Start.vue";
-
 export default {
-  name: "home",
-  components: {
-    Start
-  }
+  name: "home"
 };
 </script>
+<style lang="css">
+.start {
+  height: 77vh;
+  background-size:cover;
+  font-family: 'Black Han Sans', sans-serif;
+}
+#startContainer {
+  background-image: url("../assets/img/home_background.jpeg");
+  height: 100%;
+}
+#welc {
+  position: absolute;
+  font-weight: 700;
+  font-size: 77px;
+  color: rgb(52, 138, 191);
+}
+img {
+  padding-top: 27%;
+}
+
+#text {
+  position: absolute;
+  top: 27%;
+  right: 77px;
+  width: 457px;
+  height: 200px;
+
+  font-weight: bold;
+  color: aliceblue;
+}
+#paragraph{
+  background-color: rgba(48, 79, 128, 0.4);
+  font-size: 27px;
+}
+</style>
